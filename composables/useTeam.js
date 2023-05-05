3563c567b8e281f7b0cdde18ffa68d35e9466eac
w0dm4n <w0dm4n@hotmail.fr>
2023-05-05 23:52:19 +0200

    team

--- composables/useTeam.js
+++ composables/useTeam.js
@@ -5,14 +5,23 @@ export const useTeam = () => {
      photo: 'https://media.licdn.com/dms/image/C4E03AQFJgIIIwyREAg/profile-displayphoto-shrink_800_800/0/1658602220370?e=1685577600&v=beta&t=QLk4BNXE--p0DMmbc7EOfkmUtvhSrWEFDjN05YlwyfI',
     linkedin: 'https://www.linkedin.com/in/fr%C3%A9d%C3%A9rick-marinho-4ba80761/'
     },
-    { name: 'Rishabh Anand', role: 'team.role.cmo',
-     photo: 'https://media.licdn.com/dms/image/D5603AQFrpvEc1MpMMA/profile-displayphoto-shrink_800_800/0/1668240315468?e=1685577600&v=beta&t=IFnW1RAPIMQnMMXjPJLs9PT01yAkEXtzngd7n_5IEQg',
+    { name: 'Rishabh Anand', role: 'team.role.business',
+     photo: 'https://media.licdn.com/dms/image/D4D03AQGC1hKW14GTwg/profile-displayphoto-shrink_800_800/0/1682066940778?e=2147483647&v=beta&t=39lFNhXrxmvkNlobQrSb7LtO864jXes1HSlS76-DTc4',
      linkedin: 'https://www.linkedin.com/in/therishabhanand/'
      },
      { name: 'Brandon Hill', role: 'team.role.cm',
-     photo: 'https://i.imgur.com/B8Yjefw.jpeg',
+     photo: 'https://cdn.discordapp.com/attachments/403596139317952513/1090087371774644304/IMG_20230328_033353.jpg',
      linkedin: 'https://www.linkedin.com/in/therishabhanand/'
      },
+     { name: 'Saurav Devrani', role: 'team.role.cmo',
+     photo: 'https://i.imgur.com/NQ3JnMl.png',
+     linkedin: 'https://www.linkedin.com/in/sauravdevrani/'
+     },
+     { name: 'Vansh Jolly', role: 'team.role.pub',
+     photo: 'https://i.imgur.com/rIv4m09.png',
+     linkedin: 'https://www.linkedin.com/in/vansh-jolly-25285b18a/'
+     },
+
   ]
 
   return {
