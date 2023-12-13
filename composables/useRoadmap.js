9ee838e370a23157410c256e2f8f72a475e03c24
Triicky <mrtriickyx@gmail.com>
2023-12-13 17:37:54 +0100

    feat: update roadmap

--- composables/useRoadmap.js
+++ composables/useRoadmap.js
@@ -3,38 +3,66 @@ export const useRoadmap = () => {
     {
       title: "Q1 2023",
       list: [
-        { txt: "roadmap.q1.0", end: true },
-        { txt: "roadmap.q1.1", end: true },
-        { txt: "roadmap.q1.2", end: true },
+        { txt: "roadmap.2023.q1.0", end: true },
+        { txt: "roadmap.2023.q1.1", end: true },
+        { txt: "roadmap.2023.q1.2", end: true },
       ],
     },
     {
       title: "Q2 2023",
       list: [
-        { txt: "roadmap.q2.0", end: true },
-        { txt: "roadmap.q2.1", end: true },
-        { txt: "roadmap.q2.2" },
-        { txt: "roadmap.q2.3" },
-        { txt: "roadmap.q2.4" },
-        { txt: "roadmap.q2.5" },
-        { txt: "roadmap.q2.6" },
+        { txt: "roadmap.2023.q2.0", end: true },
+        { txt: "roadmap.2023.q2.1", end: true },
+        { txt: "roadmap.2023.q2.2", end: true },
+        { txt: "roadmap.2023.q2.3", end: true },
       ],
     },
     {
       title: "Q3 2023",
       list: [
-        { txt: "roadmap.q3.0" },
-        { txt: "roadmap.q3.1" },
-        { txt: "roadmap.q3.2" },
+        { txt: "roadmap.2023.q3.0", end: true },
+        { txt: "roadmap.2023.q3.1", end: true },
       ],
     },
     {
       title: "Q4 2023",
       list: [
-        { txt: "roadmap.q4.0" },
-        { txt: "roadmap.q4.1" },
-        { txt: "roadmap.q4.2" },
-        { txt: "roadmap.q4.3" },
+        { txt: "roadmap.2023.q4.0", end: true },
+        { txt: "roadmap.2023.q4.1", end: true },
+        { txt: "roadmap.2023.q4.2", end: true },
+      ],
+    },
+    {
+      title: "Q1 2024",
+      list: [
+        { txt: "roadmap.2024.q1.0", end: false },
+        { txt: "roadmap.2024.q1.1", end: false },
+        { txt: "roadmap.2024.q1.2", end: false },
+        { txt: "roadmap.2024.q1.3", end: false },
+        { txt: "roadmap.2024.q1.4", end: false },
+        { txt: "roadmap.2024.q1.5", end: false },
+      ],
+    },
+    {
+      title: "Q2 2024",
+      list: [
+        { txt: "roadmap.2024.q2.0", end: false },
+        { txt: "roadmap.2024.q2.1", end: false },
+        { txt: "roadmap.2024.q2.2", end: false },
+      ],
+    },
+    {
+      title: "Q3 2024",
+      list: [
+        { txt: "roadmap.2024.q3.0", end: false },
+        { txt: "roadmap.2024.q3.1", end: false },
+      ],
+    },
+    {
+      title: "Q4 2024",
+      list: [
+        { txt: "roadmap.2024.q4.0", end: false },
+        { txt: "roadmap.2024.q4.1", end: false },
       ],
     },
   ];
