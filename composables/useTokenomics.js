2d638c27075815aaff9683c96b9eb5b418d2bb2c
Triicky <mrtriickyx@gmail.com>
2023-12-13 17:50:54 +0100

    feat: update tokenomics

--- composables/useTokenomics.js
+++ composables/useTokenomics.js
@@ -1,18 +1,18 @@
 export const useTokenomics = () => {
-
   const tokenomics = [
-    { percent: 4, txt: 'tokenomics.seed' },
-    { percent: 6, txt: 'tokenomics.sale' },
-    { percent: 20, txt: 'tokenomics.team' },
-    { percent: 20, txt: 'tokenomics.marketing' },
-    { percent: 5, txt: 'tokenomics.partners' },
-    { percent: 7, txt: 'tokenomics.liquidity' },
-    { percent: 5, txt: 'tokenomics.early' },
-    { percent: 33, txt: 'tokenomics.foundation' }
-  ]
+    { percent: 5, txt: "tokenomics.seed" },
+    { percent: 8, txt: "tokenomics.sale" },
+    { percent: 10, txt: "tokenomics.dev" },
+    { percent: 5, txt: "tokenomics.early" },
+    { percent: 6, txt: "tokenomics.partners" },
+    { percent: 8, txt: "tokenomics.liquidity" },
+    { percent: 15, txt: "tokenomics.team" },
+    { percent: 5, txt: "tokenomics.advisors" },
+    { percent: 10, txt: "tokenomics.marketing" },
+    { percent: 28, txt: "tokenomics.foundation" },
+  ];
 
   return {
-    tokenomics
-  }
-
-}
+    tokenomics,
+  };
+};
